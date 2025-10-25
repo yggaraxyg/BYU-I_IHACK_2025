d4639b1121de9b1e26effbf25fab5078ce1e1172
YggaRaxyg <yggaraxyg@gmail.com>
2025-10-24 19:53:41 -0600

    Added more player methods

--- playerclasses.py
+++ playerclasses.py
@@ -1,3 +1,5 @@
+import gameover
+
 class player:
     def __init__(self, cordx, cordy, maxhp, hp, score, containter = None):
         self.cords = [cordx,cordy]
@@ -27,6 +29,24 @@ class player:
 
     def penalize(num):
         scoremod(-num)
+
+    def getscore():
+        return self.score
+
+    def gethp():
+        return self.hp
+
+    def getx():
+        return self.cords[0]
+
+    def gety():
+        return self.cords[1]
+
+    def setx(x):
+        self.cords[0]=x
+
+    def sety(y):
+        self.cords[1]=y
         
     def die():
         raise NotImplementedError
@@ -42,12 +62,45 @@ class treasure(player):
     def die():
         if (self.container !=None):
             container.remove(self)
+            del(self)
         
     def whentouching(toucher):
-        toucher.scoremod(score)
+        toucher.scoremod(self.score)
         die()
 
     def hpmod(num):
         self.hp=1
+    
+class heart(player):
+    def __init__(self, cordx, cordy, hp, container = None):
+        self.cords = [cordx, cordy]
+        self.hp = hp
+        self.maxhp = hp
+        self.score = 1
+        self.container = container
 
-class 
+    def die():
+        if (self.container !=None):
+            container.remove(self)
+            del(self)
+        
+    def whentouching(toucher):
+        toucher.hpmod(self.hp)
+        die()
+
+    def scoremod(num):
+        self.score=1
+
+    def hpmod(num):
+        self.hp=self.hp
+
+class human(player):
+    def __init__(self, cordx, cordy, maxhp, hp, score, containter = None):
+        self.cords = [cordx,cordy]
+        self.maxhp= maxhp
+        self.hp = hp
+        self.score = score
+        self.container = container
+
+    def die():
+        gameover()
