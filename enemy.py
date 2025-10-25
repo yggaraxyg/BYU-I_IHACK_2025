d75c0c403ed49198ab1df59964753412362770b0
YggaRaxyg <yggaraxyg@gmail.com>
2025-10-24 22:03:22 -0600

    just trying

--- enemy.py
+++ enemy.py
@@ -14,7 +14,7 @@ from playerclasses import player
 class Enemy(player):
     '''Base enemy class'''
     def __init__(self, cordx, cordy, maxhp, hp, score, speed=1, image_path='data/sprites/enemy.png', container=None):
-        super().__init__()
+        super().__init__(cordx, cordy, maxhp, hp, score, container = None)
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect(center=(cordx, cordy))
         self.pos = pygame.Vector2(cordx, cordy)
@@ -32,28 +32,44 @@ class Enemy(player):
         direction = pygame.Vector2(player_x, player_y) - self.pos
         if direction.length() > 0:
             direction = direction.normalize()
-            self.pos += direction
+            self.pos += direction*speed
             self.rect.center = self.pos
     
     def drop_loot(self):
-        # Placeholder for loot drop logic
-        pass
+        disable_colissions()
+        death_treasure = treasure(self.cords[0], self.cords[1], self.score, self.container)
+
+    def die():
+        if (self.container !=None):
+            drop_loot()
+            self.container.remove(self)
+            del(self)
+
 
 class Salamander(Enemy):
     # Salamander - moves toward player
-    def __init__(self, cordx, cordy, maxhp, hp, score, container = None):
-        super().__init__(cordx, cordy, maxhp, hp, score, container)
+    def __init__(self, cordx, cordy, container = None):
+        super().__init__(cordx, cordy, 20, 20, 40, 1, "data/sprites/salamander.png", container = None)
 
     def update(self, player_pos):
         self.move_towards_player(player_pos)
 
 class Eyeball(Enemy):
     # Eyeball - sits in one spot
-    def __init__(self, cordx, cordy, maxhp, hp, score, container = None):
-        super().__init__(cordx, cordy, maxhp, hp, score, container)
+    def __init__(self, cordx, cordy, container = None):
+        super().__init__(cordx, cordy, 50, 50, 40, 1, "data/sprites/florb.png", container = None)
+
+class Ogre(Enemy):
+    # Ogre - Moves slowly, very tough.
+    def __init__(self, cordx, cordy, container = None):
+        super().__init__(cordx, cordy, 400, 400, 500, 0.25, "data/sprites/florb.png", container = None)
+
+    def update(self, player_pos):
+        self.move_towards_player(player_pos)
+
 
 def create_random_enemy(cordx, cordy):
     # Make a random enemy at the given position
-    enemy_types = [Salamander, Eyeball]
+    enemy_types = [Salamander, Salamander, Salamander, Eyeball, Eyeball, Eyeball, Ogre]
     enemy_class = random.choice(enemy_types)
     return enemy_class(cordx, cordy)
