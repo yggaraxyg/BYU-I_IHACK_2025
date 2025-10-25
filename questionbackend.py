1f97f74bc1159612b6c5da824165fe6a4c25435b
YggaRaxyg <yggaraxyg@gmail.com>
2025-10-24 18:55:14 -0600

    adding

--- questionbackend.py
+++ questionbackend.py
@@ -9,26 +9,32 @@ class questionbox:
     def addQuestion(que, ans):
         self.questionlist.append([que,ans])
 
-    #gets array with question followed by three random answers and the correct one. the sixth element is which one is correct 
-    def getQuestion():
-        corans = random.choice(self.questionlist)
-        wrongans = []
-        for i in range(3):
-            acin=False
-            while(acin==False):
-                wrongan = random.choice(self.questionlist)[1]
-                if((wrongan != corans[1])and(wrongan not in wrongans)):
-                    wrongans.append(wrongan)
-                    acin=True
-        questionset = [corans[0]]
-        random.shuffle(wrongans)
-        tru = random.randint(0,3)
+    def addQuestion(que, corans, w1, w2, w3):                                                                                                                   
+        self.questionlist.append([que,corans,w1,w2,w3])                                                                                                         
+                                                                                                                                                                
+    #gets array with question followed by three random answers and the correct one. the sixth element is which one is correct                                   
+    def getQuestion():                                                                                                                                          
+        corans = random.choice(self.questionlist)                                                                                                               
+        wrongans = []                                                                                                                                          
+        if (len(corans)==2):                                                                                                                                   
+            for i in range(3):                                                                                                                                 
+                acin=False                                                                                                                                     
+                while(acin==False):                                                                                                                            
+                    wrongan = random.choice(self.questionlist)[1]                                                                                              
+                    if((wrongan != corans[1])and(wrongan not in wrongans)):                                                                                    
+                        wrongans.append(wrongan)                                                                                                               
+                        acin=True                                                                                                                              
+        else:                                                                                                                                                  
+             wrongans=[corans[2],corans[3],corans[4]]                                                                                                         
+        questionset = [corans[0]]                                                                                                                              
+        random.shuffle(wrongans)                                                                                                                               
+        tru = random.randint(0,3)                                                                                                                              
         for i in range(4):
-            if (i==tru):
-                questionset.append(corans[1])
-            else:
-                questionset.append(wrongans.pop(0))
-        questionset.append(tru)
+            if (i==tru):                                                                                                                                       
+                questionset.append(corans[1])                                                                                                                  
+            else:                                                                                                                                              
+                questionset.append(wrongans.pop(0))                                                                                                            
+        questionset.append(tru)                                                                                                                                
         return questionset
 
     #add questions from a CSV
